fc0d2cc00da7ccb69788476143b0d0d691186251
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-06-28 15:17:47 +0300

    review - problems with automatic refresh

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import figure_utils
 import prediction
 import ui
 import info_tab
+import review_tab
 
 
 def handle_model_errors(data):
@@ -56,9 +57,11 @@ def next_object(api: sly.Api, task_id, context, state, app_logger):
         api.img_ann_tool.zoom_to_figure(ann_tool_session, next_figure_id, zoom_factor=2)
         results = figure_utils.classify(state["nnId"], image_id, state["topn"], ann, next_figure_id, state["pad"])
         prediction.show(results)
+        review_tab.refresh_figure(project_id, next_figure_id)
     else:
         g.my_app.show_modal_window("All figures are visited. Select another figure or clear selection to iterate over objects again")
         prediction.hide()
+        review_tab.reset()
 
 
 @g.my_app.callback("manual_selected_image_changed")
@@ -76,6 +79,7 @@ def image_changed(api: sly.Api, task_id, context, state, app_logger):
 @sly.timeit
 @g.my_app.ignore_errors_and_show_dialog_window()
 def figure_changed(api: sly.Api, task_id, context, state, app_logger):
+    project_id = context["projectId"]
     nn_session = state["nnId"]
     if nn_session is None:
         return
@@ -88,6 +92,7 @@ def figure_changed(api: sly.Api, task_id, context, state, app_logger):
     if figure_id is None:
         sly.logger.debug("Selected figure is None")
         prediction.hide()
+        review_tab.reset()
         return
 
     project_id = context["projectId"]
@@ -97,6 +102,7 @@ def figure_changed(api: sly.Api, task_id, context, state, app_logger):
     ann = cache.get_annotation(project_id, image_id)
     results = figure_utils.classify(nn_session, image_id, state["topn"], ann, figure_id, state["pad"])
     prediction.show(results)
+    review_tab.refresh_figure(project_id, figure_id)
 
 
 @g.my_app.callback("disconnect")
@@ -122,6 +128,7 @@ def assign_to_object(api: sly.Api, task_id, context, state, app_logger):
         figure_id = context["figureId"]
         class_name = state["assignName"]
         figure_utils.assign_to_object(project_id, figure_id, class_name)
+        review_tab.refresh_figure(project_id, figure_id)
         fields = [
             {"field": "state.assignLoading", "payload": False},
             {"field": "state.previousName", "payload": class_name},
@@ -147,6 +154,7 @@ def predict(api: sly.Api, task_id, context, state, app_logger):
             ann = cache.get_annotation(project_id, image_id)
             results = figure_utils.classify(nn_session, image_id, state["topn"], ann, figure_id, state["pad"])
             prediction.show(results)
+            review_tab.refresh_figure(project_id, figure_id)
         elif apply_to == "image":
             raise NotImplementedError()
 
@@ -169,6 +177,7 @@ def mark_unknown(api: sly.Api, task_id, context, state, app_logger):
 
         if apply_to == "object":
             figure_utils._assign_tag_to_object(project_id, figure_id, g.unknown_tag_meta)
+            review_tab.refresh_figure(project_id, figure_id)
         else:
             raise NotImplementedError()
 
@@ -194,6 +203,7 @@ def mark_as_previous(api: sly.Api, task_id, context, state, app_logger):
 
         if apply_to == "object":
             figure_utils.assign_to_object(project_id, figure_id, state["previousName"])
+            review_tab.refresh_figure(project_id, figure_id)
         else:
             raise NotImplementedError()
         api.task.set_field(g.task_id, "state.assignLoading", False)
@@ -211,6 +221,8 @@ def main():
 
     g.my_app.run(data=data, state=state)
 
+#@TODO: append vs replace
+#@TODO: remove tag in review
 #@TODO: image mode
 #@TODO: get errors from serve
 if __name__ == "__main__":
---
+++ src/ui/review_tab.py
@@ -0,0 +1,42 @@
+import supervisely_lib as sly
+import globals as g
+import cache
+
+
+def init(data, state):
+    state["activeNamesReview"] = []
+    data["reviewTags"] = None
+    data["reviewTagsNames"] = None
+
+
+def init(tags, tags_names, active_names):
+    fields = [
+        {"field": "state.activeNamesReview", "payload": list(set(active_names))},
+        {"field": "data.reviewTags", "payload": tags},
+        {"field": "data.reviewTagsNames", "payload": list(set(tags_names))},
+    ]
+    g.api.task.set_fields(g.task_id, fields)
+
+
+def reset():
+    init(tags=None, tags_names=None, active_names=[])
+
+
+def refresh_figure(project_id, figure_id):
+    if figure_id is None:
+        reset()
+    else:
+        object_tags_json = g.api.advanced.get_object_tags(figure_id)
+        project_meta = cache.get_meta(project_id)
+        object_tags = sly.TagCollection.from_json(object_tags_json, project_meta.tag_metas)
+
+        review_tags = []
+        reviewTagsNames = []
+        activeNamesReview = []
+        for tag in object_tags:
+            tag: sly.Tag
+            tag_meta = project_meta.tag_metas.get(tag.meta.name)
+            if tag_meta is not None:
+                review_tags.append({**tag_meta.to_json(), "labelerLogin": tag.labeler_login})
+                reviewTagsNames.append(tag_meta.name)
+        init(review_tags, reviewTagsNames, activeNamesReview)
